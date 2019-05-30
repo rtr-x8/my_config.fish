06034b64b3431b495d025aaf07de62548ab10e57
RyutaroMatsuoka <ryu.2.24@hotmail.co.jp>
2019-05-30 16:42:37 +0900

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,2 @@
+alias gitlog="git log --graph --date=format:'%Y/%m/%d %H:%M' --pretty='format:%C(yellow)%h %C(green)%cd %C(cyan)[%an] %C(reset)%s %C(red)%d' --branches --tags --remotes"
+alias gitst="git status"
